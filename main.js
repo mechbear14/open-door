720c75798433674fa554437db9117b769eb54386
Gabe Lyu <no6xichengavenue@gmail.com>
2019-08-01 09:55:50 +0100

    Toy completed

--- main.js
+++ main.js
@@ -14,9 +14,31 @@ class DataPoint {
 
 class Point {
   constructor(dataPoint) {
+    this.dataPoint = dataPoint;
     this.id = dataPoint.id;
     this.attr = [dataPoint.a, dataPoint.b, dataPoint.c];
     this.next = [];
+  }
+
+  copy() {
+    return new Point(this.dataPoint);
+  }
+
+  render(face) {
+    let door = document.createElement("div");
+    door.classList.add("door");
+    door.dataset.id = this.id;
+    door.textContent = this.id;
+    let balloon = document.createElement("div");
+    balloon.classList.add("balloon");
+    let title = document.createElement("h1");
+    title.textContent = this.dataPoint.title;
+    let text = document.createElement("p");
+    text.textContent = this.dataPoint.text;
+    balloon.append(title);
+    balloon.append(text);
+    face.append(door);
+    face.append(balloon);
   }
 }
 
@@ -40,15 +62,29 @@ function getFakeData(n) {
   }
 }
 
+/**
+ * Game globals
+ */
 let fakeDataPoints = [];
 let points = [];
+let current;
+let coming;
+let paths = [];
+
+/**
+ * UI globals
+ */
+let world;
+let front;
+let back;
+
+/**
+ * Initialise game
+ */
 getFakeData(100);
 fakeDataPoints.forEach(dataPoint => {
   points.push(new Point(dataPoint));
 });
-let current = points[randIntBetween(0, points.length - 1)];
-let coming = null;
-let paths = [];
 for (let i = 0; i < points[0].attr.length; i++) {
   let arr = points.slice();
   paths.push(arr.sort((p1, p2) => p1.attr[i] - p2.attr[i]));
@@ -58,4 +94,67 @@ for (let i = 0; i < paths.length; i++) {
     points[paths[i][j].id].next.push(points[paths[i][j + 1].id]);
     points[paths[i][j + 1].id].next.push(points[paths[i][j].id]);
   }
+}
+current = points[randIntBetween(0, points.length - 1)];
+
+/**
+ * Initialise UI
+ */
+world = document.getElementById("world");
+front = Array.from(document.getElementsByClassName("front"))[0];
+back = Array.from(document.getElementsByClassName("back"))[0];
+current.next.forEach(point => {
+  point.render(front);
+});
+let frontDoors = Array.from(document.querySelectorAll(".front .door"));
+frontDoors.forEach(door => {
+  door.addEventListener("click", openDoor);
+});
+
+/**
+ * Events
+ */
+function openDoor(event) {
+  let opening = event.target;
+  let pointId = opening.dataset.id;
+  let frontDoors = Array.from(document.querySelectorAll(".front .door"));
+  frontDoors.forEach(door => {
+    door.removeEventListener("click", openDoor);
+  });
+  let balloons = Array.from(document.getElementsByClassName("balloon"));
+  console.log(balloons);
+  balloons.forEach(balloon => {
+    balloon.style.display = "none";
+  });
+  coming = points[pointId];
+  coming.next.forEach(point => {
+    point.render(back);
+  });
+  opening.classList.add("fall");
+  opening.addEventListener("transitionend", moveForward);
+}
+
+function moveForward(event) {
+  let opening = event.target;
+  opening.removeEventListener("transitionend", moveForward);
+  front.classList.add("out");
+  back.classList.add("front");
+  back.classList.remove("back");
+  back.addEventListener("transitionend", newRound);
+}
+
+function newRound() {
+  back.removeEventListener("transitionend", newRound);
+  let out = Array.from(document.getElementsByClassName("out"))[0];
+  world.removeChild(out);
+  front = Array.from(document.getElementsByClassName("front"))[0];
+  back = document.createElement("div");
+  back.classList.add("back");
+  back.classList.add("face");
+  world.append(back);
+  let frontDoors = Array.from(document.querySelectorAll(".front .door"));
+  frontDoors.forEach(door => {
+    door.addEventListener("click", openDoor);
+  });
+  current = coming.copy();
 }
